acf73fee1337f373f80152fd23d955928f4821cd
Xinyu Mao <maoxinyu1020@gmail.com>
2025-09-12 14:46:36 -0400

    dragdrop

---
+++ AlchemyProject/Assets/Scripts/DragDrop.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField] private Canvas canvas;
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        Debug.Log("OnPointerDown");
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnDrop(PointerEventData eventData) {
+        Debug.Log("ONDrop");
+    
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        Debug.Log("OnBeginDrag");
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        Debug.Log("OnEndGrag");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
+}
+
