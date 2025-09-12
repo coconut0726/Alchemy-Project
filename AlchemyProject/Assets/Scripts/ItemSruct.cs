1ea5c29da0ff3e47e2ae66626deb560624986a8e
Xinyu Mao <maoxinyu1020@gmail.com>
2025-09-12 18:01:19 -0400

    Item Initialization

---
+++ AlchemyProject/Assets/Scripts/ItemSruct.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSruct : MonoBehaviour
+{
+    [SerializeField] private GameObject DataBase;
+    [SerializeField] private Image iconUI;
+
+    [SerializeField] private string defaultItemID;
+    [SerializeField] private Sprite defaultIcon;
+    
+    public string itemID;
+    public string itemName;
+    public Sprite icon;
+
+    private void Awake()
+    {
+        iconUI = GetComponent<Image>();
+    }
+    private void Start()
+    {
+
+        Debug.Log("Item Initializing");
+
+        if (string.IsNullOrEmpty(defaultItemID))
+        {
+            Debug.Log("Default item empty");
+        }
+        else
+        { 
+            Debug.Log(defaultItemID);
+        }
+        if (!string.IsNullOrEmpty(defaultItemID)) {
+            if (RecipeIndex.instance == null) {
+                Debug.Log("RecipeIndex instance null");
+            }
+            var item = RecipeIndex.instance.GetItem(defaultItemID);
+ 
+            SetItem(item);
+        }
+    }
+    public void SetItem(ItemRow item)
+    {
+        Debug.Log(item.icon);
+
+        itemID = item.id;
+        itemName = item.displayName;
+        icon = item.icon;
+
+        Debug.Log(icon);
+
+        UpdateUI();
+
+    }
+
+    private void UpdateUI()
+    {
+        iconUI.sprite = icon;
+    }
+}
